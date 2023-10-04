aa3fe1ae571205e39883e3366f477f7ffd4498df
Emad <emad@gmail.com>
2023-10-04 11:07:00 +0330

    firs

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,9 @@ use App\Http\Controllers\TticketController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
+\
 */
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
